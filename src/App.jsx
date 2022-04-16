3e83d70496a832a1e417e381da3404f953881d5a
Angie <agladinop@gmail.com>
2022-04-16 00:39:11 -0500

    carousel finished, styles view about

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import TestComponent from './components/TestComponent';
-
+import { HashRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About/About.jsx";
+import LogIn from "./pages/LogIn/LogIn.jsx";
+import SignIn from "./pages/SignIn/SignIn.jsx";
 function App() {
   return (
-      <>
-        <TestComponent/>
-      </>
+    <div className="App">
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signup" element={<SignIn />} />
+        </Routes>
+      </HashRouter>
+    </div>
   );
 }
 export default App;
