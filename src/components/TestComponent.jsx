5daec25c20f89b3dbb0beb5711e2af79ba8e7d8b
Sarah <sarisp3260@gmail.com>
2022-04-18 08:09:23 -0500

    Form Profile && Header Ready

--- src/components/TestComponent.jsx
+++ src/components/TestComponent.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
-import About from './Landing/About'
 import NavUser from './UserMenu/NavUser'
-import Sidebar from './sidebar/Sidebar'
 import Profile from './UserMenu/Profile'
 
 function TestComponent() {
   return (
     <>
-      <About/>
       <NavUser/>
       <Profile/>
-      <Sidebar/>
     </>
   )
 }
---
+++ src/components/UserMenu/ItemNav.jsx
@@ -0,0 +1,75 @@
+import {FaUserCircle, FaQuestion, FaSignInAlt, FaTrophy, FaStore, FaBook, FaUsers,FaAngleDown, FaAngleUp, } from 'react-icons/fa'
+import {BsGearFill} from 'react-icons/bs'
+import logo from '../../assets/Logo/GG-LOGO-LIGHT.png'
+
+import { useState } from 'react'
+import Item from './item'
+
+const Itemnav = ({ responsive }) => {
+
+    const [drop , setDrop] = useState(false)
+
+  return (
+    <>
+        {responsive === "show" ? 
+        <>
+        <Item title="Garden">
+            <img src={logo} alt="" className='w-10'/>
+        </Item>
+        <Item title="Friends">
+            <FaUsers className='text-3xl'/>
+        </Item>
+        <Item title="Collection">
+            <FaBook className='text-3xl'/>
+        </Item>
+        <Item title="Store">
+            <FaStore className='text-3xl'/>
+        </Item>
+        <Item title="Rewards">
+            <FaTrophy className='text-3xl'/>
+        </Item>
+
+        <div className="flex items-center gap-5 text-blueDark hover:text-white/30" onClick={() => setDrop(!drop)}>
+            <FaUserCircle className='text-3xl'/>
+            <span className='text-xl'>User</span>
+            {drop === false ? <FaAngleDown className='text-3xl' onClick={()=> setDrop(!open)}/> : <FaAngleUp  className='text-3xl' onClick={()=> setDrop(!open)}/>}
+        </div> 
+        {drop && 
+        <div className='bg-purple text-white w-full p-5 rounded-lg flex flex-col gap-3'>
+            <Item title="FarmerName">
+                <FaUserCircle className='text-3xl'/>
+            </Item>
+            <Item title="Instructions">
+                <FaQuestion className='text-3xl'/>
+            </Item>
+            <Item title="Configuration">
+                <BsGearFill className='text-3xl'/>
+            </Item>
+            <Item title="Log out">
+                <FaSignInAlt className='text-3xl'/>
+            </Item>
+        </div>}
+
+
+        </>
+        : 
+        <>
+            <Item title="FarmerName">
+                <FaUserCircle className='text-3xl'/>
+            </Item>
+            <Item title="Instructions">
+                <FaQuestion className='text-3xl'/>
+            </Item>
+            <Item title="Configuration">
+                <BsGearFill className='text-3xl'/>
+            </Item>
+            <Item title="Log out">
+                <FaSignInAlt className='text-3xl'/>
+            </Item>
+        </>}
+
+    </>
+  )
+}
+
+export default Itemnav
